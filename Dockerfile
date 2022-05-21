06dd761a0c9940cd603cbbeb1d806bf9282c283c
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-05-22 05:39:47 +0800

    code improvements

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ COPY ./content /workdir/
 
 RUN apk add --no-cache curl caddy jq bash findutils runit apache2-utils tzdata ttyd \
     && chmod +x /workdir/service/*/run /workdir/service/*/log/run /workdir/*.sh \
-    && /workdir/install.sh
+    && /workdir/install.sh \
+    && rm -f /workdir/install.sh
 
 ENTRYPOINT ["sh","/workdir/entrypoint.sh"]
--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 mkdir -p /mnt/data/config /mnt/data/qbit_downloads /mnt/data/aria2_downloads /mnt/data/videos
-BACKUP=$(curl --retry 4 https://${CLOUDFLARE_WORKERS_HOST}/backup?key=${CLOUDFLARE_WORKERS_KEY} | jq .value | sed 's/\"//g')
+BACKUP=$(curl --retry 4 https://${CLOUDFLARE_WORKERS_HOST}/backup?key=${CLOUDFLARE_WORKERS_KEY} | jq .value)
 DIR_TMP="$(mktemp -d)"
-echo ${BACKUP} | base64 -d  >${DIR_TMP}/backup.tar.gz
+echo ${BACKUP} | base64 -d >${DIR_TMP}/backup.tar.gz
 tar -zxf ${DIR_TMP}/backup.tar.gz -C /mnt/data
 rm -rf ${DIR_TMP}
 
