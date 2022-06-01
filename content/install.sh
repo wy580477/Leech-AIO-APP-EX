087d16104d1f4fbaa43f472cb6dfb559f154be0c
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-06-01 16:54:25 +0800

    Add lite version container

--- content/install.sh
+++ content/install.sh
@@ -43,3 +43,8 @@ wget -O - https://github.com/filebrowser/filebrowser/releases/latest/download/li
 wget -O - https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz | tar -zxf - -C /usr/bin
 
 rm -rf ${DIR_TMP}
+
+# Remove pyLoad related files
+if [ "${PYLOAD_INSTALL}" = "Disable" ]; then
+  rm -rf /workdir/service/pyLoad /workdir/homer_conf/pyload.png
+fi
