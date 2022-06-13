ae99242bbd52b798f2dd9874d3528ade9ea9cf19
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-06-13 13:04:11 +0800

    Add independent upload folder for each downloader

--- content/pyload_to_rclone.sh
+++ content/pyload_to_rclone.sh
@@ -6,13 +6,21 @@ DATE_TIME() {
 
 UPLOAD_MODE="$(grep ^pyload-download-finished-upload-mode /mnt/data/config/script.conf | cut -d= -f2-)"
 DRIVE_NAME="$(grep ^drive-name /mnt/data/config/script.conf | cut -d= -f2-)"
-DRIVE_DIR="$(grep ^drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
 
 DRIVE_NAME_AUTO="$(sed -n '1p' /mnt/data/config/rclone.conf | sed "s/.*\[//g;s/\].*//g;s/\r$//")"
 if [ "${DRIVE_NAME}" = "auto" ]; then
     DRIVENAME=${DRIVE_NAME_AUTO}
 else
     DRIVENAME=${DRIVE_NAME}
+fi
+
+GLOBAL_DRIVE_DIR="$(grep ^drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
+PYLOAD_DRIVE_DIR="$(grep ^pyload-drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
+
+if [ "${PYLOAD_DRIVE_DIR}" = "" ]; then
+    DRIVE_DIR=${GLOBAL_DRIVE_DIR}/pyload
+else
+    DRIVE_DIR=${PYLOAD_DRIVE_DIR}
 fi
 
 REMOTE_PATH="${DRIVENAME}:${DRIVE_DIR}"
