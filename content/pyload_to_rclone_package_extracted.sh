c2b69bcd959f0d1e409cf5e99f4a0e59c69eaef0
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-07-03 07:06:06 +0800

    Rewrite auto-upload scripts

--- content/pyload_to_rclone_package_extracted.sh
+++ content/pyload_to_rclone_package_extracted.sh
@@ -1,44 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Created by wy580477 for customized container <https://github.com/wy580477>
+#
 
-DATE_TIME() {
-    date +"%m/%d %H:%M:%S"
+LOCAL_PATH="$3"
+PACKAGE_NAME="$2"
+source /workdir/script_core.sh
+
+DEFINITION_PATH() {
+    UPLOAD_MODE="$(grep ^pyload-package-extracted-upload-mode /mnt/data/config/script.conf | cut -d= -f2-)"
+    DOWNLOAD_DIR="$(grep ^'.*folder storage_folder' /workdir/.pyload/settings/pyload.cfg | cut -d= -f2- | sed "s/\r$//")"
+    PYLOAD_DRIVE_DIR="$(grep ^pyload-drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
+    if [[ "${PYLOAD_DRIVE_DIR}" =~ :/ ]]; then
+        REMOTE_PATH="${PYLOAD_DRIVE_DIR}/${PACKAGE_NAME}"
+    else
+        if [ "${PYLOAD_DRIVE_DIR}" = "" ]; then
+            DRIVE_DIR=${GLOBAL_DRIVE_DIR}/pyload
+        else
+            DRIVE_DIR=${PYLOAD_DRIVE_DIR}
+        fi
+        REMOTE_PATH="${DRIVENAME}:${DRIVE_DIR}/${PACKAGE_NAME}"
+    fi
 }
 
-UPLOAD_MODE="$(grep ^pyload-package-extracted-upload-mode /mnt/data/config/script.conf | cut -d= -f2-)"
-DELETE_EMPTY_DIR="$(grep ^delete-empty-dir /mnt/data/config/script.conf | cut -d= -f2-)"
-DRIVE_NAME="$(grep ^drive-name /mnt/data/config/script.conf | cut -d= -f2-)"
-PYLOAD_DOWNLOAD_DIR="$(grep ^'.*folder storage_folder' /workdir/.pyload/settings/pyload.cfg | cut -d= -f2- | sed "s/\r$//")"
-
-DRIVE_NAME_AUTO="$(sed -n '1p' /mnt/data/config/rclone.conf | sed "s/.*\[//g;s/\].*//g;s/\r$//")"
-if [ "${DRIVE_NAME}" = "auto" ]; then
-    DRIVENAME=${DRIVE_NAME_AUTO}
-else
-    DRIVENAME=${DRIVE_NAME}
-fi
-
-GLOBAL_DRIVE_DIR="$(grep ^drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
-PYLOAD_DRIVE_DIR="$(grep ^pyload-drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
-
-if [ "${PYLOAD_DRIVE_DIR}" = "" ]; then
-    DRIVE_DIR=${GLOBAL_DRIVE_DIR}/pyload
-else
-    DRIVE_DIR=${PYLOAD_DRIVE_DIR}
-fi
-
-REMOTE_PATH="${DRIVENAME}:${DRIVE_DIR}"
-
-if [ "${UPLOAD_MODE}" = "disable" ]; then
-    echo "$(DATE_TIME) [INFO] Auto-upload to Rclone remote disabled"
-else
-    JOB_ID="$(curl -s -u ${GLOBAL_USER}:${GLOBAL_PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"$3"'","dstFs":"'"${REMOTE_PATH}"'/'"$2"'","_async":"true"}' 'localhost:61802/sync/'${UPLOAD_MODE}'' | jq .jobid | sed 's/\"//g')"
-    if [ "${JOB_ID}" != "" ]; then
-        echo "$(DATE_TIME) [INFO] Successfully send job to rclone: $3 -> ${REMOTE_PATH}"
-        curl -s -u ${GLOBAL_USER}:${GLOBAL_PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"jobid":"'"${JOB_ID}"'"}' 'localhost:61802/job/status'
+UPLOAD_TASK() {
+    if [ "${UPLOAD_MODE}" = "disable" ]; then
+        echo "$(DATE_TIME) [INFO] Auto-upload to Rclone remote disabled"
     else
-        echo "$(DATE_TIME) [ERROR] Failed to send job to rclone: $3"
+        UPLOAD_FOLDER
     fi
-fi
+}
 
-if [[ "${DELETE_EMPTY_DIR}" = "true" ]]; then
-    find ${PYLOAD_DOWNLOAD_DIR} -depth -mindepth 1 -type d -empty -exec rm -vrf {} \; 2>/dev/null
-fi
+GET_PATH
+DEFINITION_PATH
+UPLOAD_TASK
+CLEAN_EMPTY_DIR
--- content/ytdlptorclone.sh
+++ content/ytdlptorclone.sh
@@ -1,41 +1,40 @@
-#!/bin/sh
+#!/bin/bash
+#
+# Created by wy580477 for customized container <https://github.com/wy580477>
+#
 
-DATE_TIME() {
-    date +"%m/%d %H:%M:%S"
+LOCAL_PATH="$1"
+source /workdir/script_core.sh
+
+DEFINITION_PATH() {
+    UPLOAD_MODE="$(grep ^ytdlp-upload-mode /mnt/data/config/script.conf | cut -d= -f2-)"
+    YTDLP_DRIVE_DIR="$(grep ^ytdlp-drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
+    FILE_PATH=$(echo ${LOCAL_PATH} | sed 's:[^/]*$::')
+    FILE_NAME=$(basename "${LOCAL_PATH}")
+    mv "${LOCAL_PATH}" "${FILE_PATH}""${FILE_NAME}"
+    if [[ "${YTDLP_DRIVE_DIR}" =~ :/ ]]; then
+        REMOTE_PATH="${YTDLP_DRIVE_DIR}"
+    else
+        if [ "${YTDLP_DRIVE_DIR}" = "" ]; then
+            DRIVE_DIR=${GLOBAL_DRIVE_DIR}/yt-dlp
+        else
+            DRIVE_DIR=${YTDLP_DRIVE_DIR}
+        fi
+        REMOTE_PATH="${DRIVENAME}:${DRIVE_DIR}"
+    fi
 }
 
-UPLOAD_MODE="$(grep ^ytdlp-upload-mode /mnt/data/config/script.conf | cut -d= -f2-)"
-DRIVE_NAME="$(grep ^drive-name /mnt/data/config/script.conf | cut -d= -f2-)"
-
-DRIVE_NAME_AUTO="$(sed -n '1p' /mnt/data/config/rclone.conf | sed "s/.*\[//g;s/\].*//g;s/\r$//")"
-if [ "${DRIVE_NAME}" = "auto" ]; then
-    DRIVENAME=${DRIVE_NAME_AUTO}
-else
-    DRIVENAME=${DRIVE_NAME}
-fi
-
-GLOBAL_DRIVE_DIR="$(grep ^drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
-YTDLP_DRIVE_DIR="$(grep ^ytdlp-drive-dir /mnt/data/config/script.conf | cut -d= -f2-)"
-
-if [ "${YTDLP_DRIVE_DIR}" = "" ]; then
-    DRIVE_DIR=${GLOBAL_DRIVE_DIR}/yt-dlp
-else
-    DRIVE_DIR=${YTDLP_DRIVE_DIR}
-fi
-
-REMOTE_PATH="${DRIVENAME}:${DRIVE_DIR}"
-FILEPATH=$(echo $1 | sed 's:[^/]*$::')
-FILENAME=$(basename "$1")
-mv "$1" "${FILEPATH}""${FILENAME}"
-
-if [ "${UPLOAD_MODE}" = "disable" ]; then
-    echo "$(DATE_TIME) [INFO] Auto-upload to Rclone remote disabled"
-else
-    JOB_ID="$(curl -s -u ${GLOBAL_USER}:${GLOBAL_PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"srcFs":"'"${FILEPATH}"'","srcRemote":"'"${FILENAME}"'","dstFs":"'"${REMOTE_PATH}"'","dstRemote":"'"${FILENAME}"'","_async":"true"}' 'localhost:61802/operations/'${UPLOAD_MODE}'file' | jq .jobid | sed 's/\"//g')"
-    if [ "${JOB_ID}" != "" ]; then
-        echo "$(DATE_TIME) [INFO] Successfully send job to rclone: $1 -> ${REMOTE_PATH}"
-        curl -s -u ${GLOBAL_USER}:${GLOBAL_PASSWORD} -H "Content-Type: application/json" -f -X POST -d '{"jobid":"'"${JOB_ID}"'"}' 'localhost:61802/job/status'
+UPLOAD_TASK() {
+    if [ "${UPLOAD_MODE}" = "disable" ]; then
+        echo "$(DATE_TIME) [INFO] Auto-upload to Rclone remote disabled"
     else
-        echo "$(DATE_TIME) [ERROR] Failed to send job to rclone: $1"
+        UPLOAD_FILE
     fi
-fi
+    if [ "${JOB_ID}" = "" ]; then
+        exit 1
+    fi
+}
+
+GET_PATH
+DEFINITION_PATH
+UPLOAD_TASK
