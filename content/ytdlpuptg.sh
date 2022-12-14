50ba25c31b1b16ca6edca22e9820c9bd76cd6407
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-12-14 17:06:48 +0800

    Update ytdlpuptg.sh

--- content/ytdlpuptg.sh
+++ content/ytdlpuptg.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 
 source /workdir/script_core.sh
-APP=Telegram
-MSG_PATH="$@"
+TELEGRAM_DEST="$(grep ^telegram-dest "${SCRIPT_CONF}" | cut -d= -f2-)"
 
-telegram-upload -d "$1"
+SEND_TO_TG() {
+    if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
+        SEND_TG_MSG yt-dlp "$1 下载已完成并发送上传任务至 Telegram"
+    else
+        SEND_TG_MSG yt-dlp "$1 download completed and send upload job to Telegram"
+    fi
+}
+
+if [ "${TELEGRAM_DEST}" != "" ]; then
+    DEST_PARAM="--to ${TELEGRAM_DEST}"
+fi
+
+SEND_TO_TG
+telegram-upload ${DEST_PARAM} -d "$1"
 EXIT_CODE=$?
 if [ ${EXIT_CODE} -eq 0 ]; then
     echo "[INFO] Successfully upload file to Telegram: $1"
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        SEND_TG_MSG "${APP}" "[INFO] Telegram 上传任务已成功: $1"
+        SEND_TG_MSG Telegram "[INFO] Telegram 上传任务已成功: $1"
     else
-        SEND_TG_MSG "${APP}" "[INFO] File successfully uploaded to Telegram : $1"
+        SEND_TG_MSG Telegram "[INFO] File successfully uploaded to Telegram : $1"
     fi
 else
     echo "[ERROR] Failed to upload file to Telegram: $1"
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        SEND_TG_MSG "${APP}" "[ERROR] Telegram 上传任务失败: $1"
+        SEND_TG_MSG Telegram "[ERROR] Telegram 上传任务失败: $1"
     else
-        SEND_TG_MSG "${APP}" "[ERROR] Failed to upload file to Telegram: $1"
+        SEND_TG_MSG Telegram "[ERROR] Failed to upload file to Telegram: $1"
     fi
 fi 
