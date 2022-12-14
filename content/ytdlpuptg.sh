c3419d77a1e723ba079827d860b0e28fd1e4233a
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-12-14 19:37:31 +0800

    telegram-upload scripts fixes

--- content/ytdlpuptg.sh
+++ content/ytdlpuptg.sh
@@ -1,13 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 source /workdir/script_core.sh
 TELEGRAM_DEST="$(grep ^telegram-dest "${SCRIPT_CONF}" | cut -d= -f2-)"
+FILE_NAME="$(basename "$1")"
 
 SEND_TO_TG() {
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        SEND_TG_MSG yt-dlp "$1 下载已完成并发送上传任务至 Telegram"
+        SEND_TG_MSG yt-dlp "${FILE_NAME} 下载已完成并发送上传任务至 Telegram"
     else
-        SEND_TG_MSG yt-dlp "$1 download completed and send upload job to Telegram"
+        SEND_TG_MSG yt-dlp "${FILE_NAME} download completed and send upload job to Telegram"
     fi
 }
 
@@ -15,6 +16,7 @@ if [ "${TELEGRAM_DEST}" != "" ]; then
     DEST_PARAM="--to ${TELEGRAM_DEST}"
 fi
 
+GET_PATH
 SEND_TO_TG
 telegram-upload ${DEST_PARAM} -d "$1"
 EXIT_CODE=$?
