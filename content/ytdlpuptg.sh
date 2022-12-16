744aae0f1750d2f8388e56928df9762730ffc1c2
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-12-16 21:32:23 +0800

    Improve telegram upload script

--- content/ytdlpuptg.sh
+++ content/ytdlpuptg.sh
@@ -24,20 +24,32 @@ if [ "${TELEGRAM_MODE}" = "move" ]; then
     UP_MODE_PARAM="-d"
 fi
 SEND_TO_TG
-telegram-upload ${DEST_PARAM} ${PROXY_PARAM} ${UP_MODE_PARAM} "$1"
-EXIT_CODE=$?
-if [ ${EXIT_CODE} -eq 0 ]; then
-    echo "[INFO] Successfully upload file to Telegram: $1"
-    if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-        SEND_TG_MSG Telegram "[INFO] Telegram 上传任务已成功: $1"
+RETRY=0
+RETRY_NUM=10
+while [ ${RETRY} -le ${RETRY_NUM} ]; do
+    [ ${RETRY} != 0 ] && (
+        echo -e "$(DATE_TIME) ${ERROR} Telegram Upload failed! Retry ${RETRY}/${RETRY_NUM} ..."
+    )
+    telegram-upload ${DEST_PARAM} ${PROXY_PARAM} ${UP_MODE_PARAM} "$1"
+    EXIT_CODE=$?
+    if [ ${EXIT_CODE} -eq 0 ]; then
+        echo "$(DATE_TIME) [INFO] Successfully upload file to Telegram: $1"
+        if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
+            SEND_TG_MSG Telegram "[INFO] Telegram 上传任务已成功: $1"
+        else
+            SEND_TG_MSG Telegram "[INFO] File successfully uploaded to Telegram : $1"
+        fi
+        break
     else
-        SEND_TG_MSG Telegram "[INFO] File successfully uploaded to Telegram : $1"
+        RETRY=$((${RETRY} + 1))
+        sleep 10
     fi
-else
-    echo "[ERROR] Failed to upload file to Telegram: $1"
+done
+if [[ "${RETRY}" -eq 2 ]] && [[ ! "${EXIT_CODE}" -eq 0 ]]; then
+    echo "$(DATE_TIME) [ERROR] Failed to upload file to Telegram: $1"
     if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
         SEND_TG_MSG Telegram "[ERROR] Telegram 上传任务失败: $1"
     else
         SEND_TG_MSG Telegram "[ERROR] Failed to upload file to Telegram: $1"
     fi
-fi 
+fi
