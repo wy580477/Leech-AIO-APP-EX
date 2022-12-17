ee9c859d2782de4786aeb3c1118ebc10d97274bf
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-12-17 09:13:03 +0800

    Change to qBittorrent Enhanced Edition

--- content/install.sh
+++ content/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -x
+set -xe
 
 DIR_TMP="$(mktemp -d)"
 
@@ -8,21 +8,21 @@ OS_type="$(uname -m)"
 case "$OS_type" in
   x86_64|amd64)
     OS_type='amd64'
-    OS_type2='x86_64'
+    OS_type2='x86_64-linux-musl_static'
     OS_type3='amd64'
     OS_type4='amd64'
     OS_type5='amd64'
     ;;
   aarch64|arm64)
     OS_type='arm64'
-    OS_type2='aarch64'
+    OS_type2='aarch64-linux-musl_static'
     OS_type3='arm64'
     OS_type4='arm64'
     OS_type5='arm64'
     ;;
   arm*)
     OS_type='arm-v7'
-    OS_type2='armv7'
+    OS_type2='arm-linux-musleabi_static'
     OS_type3='armv7'
     OS_type4='armhf'
     OS_type5='arm32v7'
@@ -38,8 +38,8 @@ wget -O - https://downloads.rclone.org/rclone-current-linux-${OS_type}.zip | bus
 install -m 755 ${DIR_TMP}/*/rclone /usr/bin/rclone
 
 # Install qBit
-wget -O /usr/bin/qbit https://github.com/userdocs/qbittorrent-nox-static/releases/download/release-4.4.5_v2.0.8/${OS_type2}-qbittorrent-nox
-chmod +x /usr/bin/qbit
+wget -qO - https://github.com/c0re100/qBittorrent-Enhanced-Edition/releases/download/release-4.4.5.10/qbittorrent-enhanced-nox_${OS_type2}.zip | busybox unzip -qd ${DIR_TMP} -
+install -m 755 ${DIR_TMP}/qbittorrent-nox /usr/bin/qbittorrent-nox
 
 # Install Filebrowser
 wget -O - https://github.com/filebrowser/filebrowser/releases/latest/download/linux-${OS_type3}-filebrowser.tar.gz | tar -zxf - -C /usr/bin
