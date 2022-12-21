9d2b7d36c52330f1890c1afc9ca987931698b371
wy580477 <98247050+wy580477@users.noreply.github.com>
2022-12-21 20:01:17 +0800

    Keep sub folder when uploading via Rclone

--- content/ytdlptorclone.sh
+++ content/ytdlptorclone.sh
@@ -14,17 +14,19 @@ DEFINITION_PATH() {
     FILE_NAME=$(basename "${LOCAL_PATH}")
     MSG_PATH="${LOCAL_PATH}"
     FOLDER_NAME="${FILE_NAME}"
+    DEST_PATH_SUFFIX="${FILE_PATH#"${DOWNLOAD_DIR%/}"}"
     mv "${LOCAL_PATH}" "${FILE_PATH}""${FILE_NAME}"
     if [[ "${YTDLP_DRIVE_DIR}" =~ :/ ]]; then
-        REMOTE_PATH="${YTDLP_DRIVE_DIR}"
+        REMOTE_PATH_PREFIX="${YTDLP_DRIVE_DIR}"
     else
         if [ "${YTDLP_DRIVE_DIR}" = "" ]; then
             DRIVE_DIR=${GLOBAL_DRIVE_DIR}/yt-dlp
         else
             DRIVE_DIR=${YTDLP_DRIVE_DIR}
         fi
-        REMOTE_PATH="${DRIVENAME}:${DRIVE_DIR}"
+        REMOTE_PATH_PREFIX="${DRIVENAME}:${DRIVE_DIR}"
     fi
+    REMOTE_PATH="${REMOTE_PATH_PREFIX}${DEST_PATH_SUFFIX}"
 }
 
 UPLOAD_TASK() {
