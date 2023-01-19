e383b90c09395091d2bc3f8f2b33ea2c4913198c
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-01-19 08:30:26 +0800

    Update entrypoint.sh

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -3,7 +3,7 @@
 # Write container start time
 echo $(date +%s) > /workdir/container_start_time
 
-mkdir -p /mnt/data/config /mnt/data/qbit_downloads /mnt/data/aria2_downloads /mnt/data/videos
+mkdir -p /mnt/data/config /mnt/data/qbit_downloads /mnt/data/aria2_downloads /mnt/data/videos 2>/dev/null
 
 if [ ! -f "/mnt/data/config/script.conf" ]; then
        cp /workdir/script.conf /mnt/data/config/script.conf
