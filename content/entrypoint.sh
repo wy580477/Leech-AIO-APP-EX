9d3dc16499e1aa16bc5c1cbbce50c4ec44c7f898
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-01-25 17:02:41 +0800

    Improve Error handling for Cloudflare Workers
    Now backup data won't be over-written after Workers connection failed.

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -1,30 +1,26 @@
 #!/bin/sh
 
 # Write dyno start time
-echo $(date +%s) > /workdir/dyno_start_time
-
-mkdir -p /mnt/data/config /mnt/data/qbit_downloads /mnt/data/aria2_downloads /mnt/data/videos /workdir/.pyload /mnt/data/.cache
+echo $(date +%s) >/workdir/dyno_start_time
 
 # Restore backup
-BACKUP=$(curl -4 --retry 4 https://${CLOUDFLARE_WORKERS_HOST}/backup?key=${CLOUDFLARE_WORKERS_KEY} | jq .value)
-DIR_TMP="$(mktemp -d)"
-echo ${BACKUP} | base64 -d >${DIR_TMP}/backup.tar.gz
-tar -zxf ${DIR_TMP}/backup.tar.gz -C /mnt/data
-mv /mnt/data/config/settings /workdir/.pyload 2>/dev/null
-mv /mnt/data/config/gallery-dl /mnt/data/.cache 2>/dev/null
-mv /mnt/data/config/.config /mnt/data 2>/dev/null
-rm -rf ${DIR_TMP}
-
-if [ ! -f "/mnt/data/config/script.conf" ]; then
-       cp /workdir/script.conf /mnt/data/config/script.conf
-fi
+RESTORE_BACKUP() {
+    BACKUP=$(curl -4 --retry 4 https://${CLOUDFLARE_WORKERS_HOST}/backup?key=${CLOUDFLARE_WORKERS_KEY} | jq .value)
+    DIR_TMP="$(mktemp -d)"
+    echo ${BACKUP} | base64 -d >${DIR_TMP}/backup.tar.gz
+    tar -zxf ${DIR_TMP}/backup.tar.gz -C /mnt/data
+    mv /mnt/data/config/settings /workdir/.pyload 2>/dev/null
+    mv /mnt/data/config/gallery-dl /mnt/data/.cache 2>/dev/null
+    mv /mnt/data/config/.config /mnt/data 2>/dev/null
+    rm -rf ${DIR_TMP}
+}
 
 SEND_TG_MSG() {
     SCRIPT_CONF="/mnt/data/config/script.conf"
     TELEGRAM_BOT_TOKEN="$(grep ^telegram-bot-token "${SCRIPT_CONF}" | cut -d= -f2-)"
     TELEGRAM_CHAT_ID="$(grep ^telegram-chat-id "${SCRIPT_CONF}" | cut -d= -f2-)"
     TG_PROXY="$(grep ^telegram-proxy "${SCRIPT_CONF}" | cut -d= -f2-)"
-    if [ "${TELEGRAM_CHAT_ID}" != "" ]; then        
+    if [ "${TELEGRAM_CHAT_ID}" != "" ]; then
         if [ "${TG_PROXY}" != "" ]; then
             PROXY_PARAM="-x ${TG_PROXY}"
         fi
@@ -43,5 +39,22 @@ SEND_TG_MSG() {
 }
 
 SEND_TG_MSG
+mkdir -p /mnt/data/config /mnt/data/qbit_downloads /mnt/data/aria2_downloads /mnt/data/videos /workdir/.pyload /mnt/data/.cache
+WORKER_STATUS=$(curl -4 --retry 4 https://${CLOUDFLARE_WORKERS_HOST} | jq .status | sed "s|\"||g")
+
+if [ "${WORKER_STATUS}" = "Running" ]; then
+    RESTORE_BACKUP
+else
+    if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
+        echo "无法连接 Cloudflare Workers"
+    else
+        echo "Cloudflare Workers is not working"
+    fi
+    touch /workdir/workers_fail.lock
+fi
+
+if [ ! -f "/mnt/data/config/script.conf" ]; then
+    cp /workdir/script.conf /mnt/data/config/script.conf
+fi
 
 exec runsvdir -P /etc/service
