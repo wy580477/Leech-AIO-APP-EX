af7011579040a33c9ca70b5199f90198e66013fa
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-01-29 16:29:42 +0800

    Custom Telegram notification title

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,11 @@ RUN apk add --no-cache curl caddy jq bash runit tzdata ttyd p7zip findutils wget
     && rm -rf /workdir/install.sh /tmp/* ${HOME}/.cache ${HOME}/.cargo
 
 ENV PORT=3000
+ENV GLOBAL_USER=admin
+ENV GLOBAL_PASSWORD=password
+ENV GLOBAL_LANGUAGE=en
+ENV GLOBAL_PORTAL_PATH=/mypath
+ENV TZ=UTC
 
 EXPOSE 3000
 
--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -19,19 +19,19 @@ SEND_TG_MSG() {
     SCRIPT_CONF="/mnt/data/config/script.conf"
     TELEGRAM_BOT_TOKEN="$(grep ^telegram-bot-token "${SCRIPT_CONF}" | cut -d= -f2-)"
     TELEGRAM_CHAT_ID="$(grep ^telegram-chat-id "${SCRIPT_CONF}" | cut -d= -f2-)"
+    TELEGRAM_TITLE="$(grep ^telegram-notification-title "${SCRIPT_CONF}" | cut -d= -f2-)"
     TG_PROXY="$(grep ^telegram-proxy "${SCRIPT_CONF}" | cut -d= -f2-)"
     if [ "${TELEGRAM_CHAT_ID}" != "" ]; then
         if [ "${TG_PROXY}" != "" ]; then
             PROXY_PARAM="-x ${TG_PROXY}"
         fi
         if [ "${GLOBAL_LANGUAGE}" = "chs" ]; then
-            msgbody="Dyno 已启动"
+            msgbody="容器已启动"
         else
-            msgbody="Dyno started"
+            msgbody="Container started"
         fi
-        title="Heroku"
         timestamp="$(date +"%m/%d %H:%M:%S")"
-        msg="$title $timestamp\n$(echo "$msgbody" | sed -e 's|\\|\\\\|g' -e 's|\n|\\n|g' -e 's|\t|\\t|g' -e 's|\"|\\"|g')"
+        msg="$TELEGRAM_TITLE $timestamp\n$(echo "$msgbody" | sed -e 's|\\|\\\\|g' -e 's|\n|\\n|g' -e 's|\t|\\t|g' -e 's|\"|\\"|g')"
         entities="[{\"offset\":0,\"length\":${#title},\"type\":\"bold\"},{\"offset\":$((${#title} + 1)),\"length\":${#timestamp},\"type\":\"italic\"}]"
         data="{\"chat_id\":\"$TELEGRAM_CHAT_ID\",\"text\":\"$msg\",\"entities\":$entities,\"disable_notification\": true}"
         curl -s "${PROXY_PARAM}" -o /dev/null -H 'Content-Type: application/json' -X POST -d "$data" https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
